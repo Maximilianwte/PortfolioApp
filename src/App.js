2bcf5a5592d8cffdee75882f8b89703d78c959f6
Maximilian Witte <maximilianwitte@outlook.de>
2018-06-29 16:06:20 +0200

    Intiliazing & Basic Styling for Context and Portfolio

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter } from "react-router-dom";
+import 'bootstrap';
+
+import { connect } from "react-redux";
+
+import './App.css';
+
+import ScrollToTop from "./ScrollToTop";
+import Context from "./Components/Context/Context";
+import Portfolio from "./Components/Portfolio/Portfolio";
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <ScrollToTop>
+       <div className="App">
+          <Route exact path="/" component={Context} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </div>
+      </ScrollToTop>
+    </BrowserRouter>
+    );
+  }
+}
+
+export default App;
